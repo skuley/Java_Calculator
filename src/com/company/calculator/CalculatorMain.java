673bd1cd58e98eeacdb777fb6850ffc6ffd018be
skuley <skuleyandpotat@gmail.com>
2021-01-10 15:48:52 +0900

    반복적인 연산 기능 추가 ㄴ end 입력시 Scanner 종료

--- src/com/company/calculator/CalculatorMain.java
+++ src/com/company/calculator/CalculatorMain.java
@@ -13,12 +13,36 @@ public class CalculatorMain {
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
 
+        // 1. 원하는 숫자를 입력하고 연산 기능을 결정하는 입력을 받는다
+
         while(true){
+            System.out.println("어떤 연산을 하시겠습니까?");
+            String input = sc.next();
+
+            switch (input){
+                case "+":
+                    System.out.println(num1 + " + " + num2 + " = " + (num1 + num2) );
+                    break;
+
+                case "-":
+                    System.out.println(num1 + " - " + num2 + " = " + (num1 - num2) );
+                    break;
+
+                case "*":
+                    System.out.println(num1 + " * " + num2 + " = " + (num1 * num2) );
+                    break;
+
+                case "/":
+                    System.out.println(num1 + " / " + num2 + " = " + (num1 / num2) );
+                    break;
+
+                case "end":
+                    sc.close();
+                    return;
+
+            }
 
         }
-
-
-
 
     } // main()
 } // Calculator
